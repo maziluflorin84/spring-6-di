8eb975afeeef4181a6d79017acefbd118394d5c5
Florin <maziluflorin84@outlook.com>
2024-09-29 19:01:04 +0300

    37. Using Qualifiers

---
+++ src/main/java/florin/springframework/spring6di/services/GreetingServicePropertyInjected.java
@@ -0,0 +1,11 @@
+package florin.springframework.spring6di.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("propertyGreetingService")
+public class GreetingServicePropertyInjected implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Friends don't let Friends do property injection!!!";
+    }
+}
