03536623512e683bcb30e23225dcfe68107e9ef5
Florin <maziluflorin84@outlook.com>
2024-09-29 17:54:38 +0300

    35. Dependency Injection using Spring Framework

---
+++ src/test/java/florin/springframework/spring6di/controllers/MyControllerTest.java
@@ -0,0 +1,13 @@
+package florin.springframework.spring6di.controllers;
+
+import org.junit.jupiter.api.Test;
+
+class MyControllerTest {
+
+    @Test
+    void sayHello() {
+        MyController myController = new MyController();
+
+        System.out.println(myController.sayHello());
+    }
+}
